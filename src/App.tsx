dead9a1f92f155e8210dcf5bc5995f73342ec0b1
Sule Tunkara <st.tunkara@gmail.com>
2025-07-17 13:15:57 +0100

    Replace TV show titles with covers #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,61 @@
 import React from 'react';
 import './App.css';
 import Daredevil from './daredevil-cover.jpg';
+import MyNameIsEarl from './my-name-is-earl.jpg';
+import Ozark from './ozark.jpg';
+import SweetHome from './sweet-home.jpg';
+import TheMentalist from './the-mentalist.jpg';
+import Westworld from './westworld.jpg';
 
 function App() {
   return (
     <div className="App">
-      <h1>Kodflix</h1>
-      <img
-        src={Daredevil}
-        alt="Favourite Movie Cover"
-        className="image-cover"
-      />
+      <div className="image-cover-row">
+        <div className="image-cover-row-item">
+          <img
+            src={Daredevil}
+            alt="Favourite Movie Cover"
+            className="image-cover"
+          />
+        </div>
+        <div className="image-cover-row-item">
+          <img
+            src={MyNameIsEarl}
+            alt="Favourite Movie Cover"
+            className="image-cover"
+          />
+        </div>
+        <div className="image-cover-row-item">
+          <img
+            src={Ozark}
+            alt="Favourite Movie Cover"
+            className="image-cover"
+          />
+        </div>
+      </div>
+      <div className="image-cover-row">
+        <div className="image-cover-row-item">
+          <img
+            src={SweetHome}
+            alt="Favourite Movie Cover"
+            className="image-cover"
+          />
+        </div>
+        <div className="image-cover-row-item">
+          <img
+            src={TheMentalist}
+            alt="Favourite Movie Cover"
+            className="image-cover"
+          />
+        </div>
+        <div className="image-cover-row-item">
+          <img
+            src={Westworld}
+            alt="Favourite Movie Cover"
+            className="image-cover"
+          />
+        </div>
+      </div>
     </div>
   );
 }
