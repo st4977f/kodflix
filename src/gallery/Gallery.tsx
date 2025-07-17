38c167d188d2d1beb95c270720fd5b6c58821c9d
Sule Tunkara <st.tunkara@gmail.com>
2025-07-17 15:25:41 +0100

    Improve the gallery layout #11

--- src/gallery/Gallery.tsx
+++ src/gallery/Gallery.tsx
@@ -11,46 +11,42 @@ import Westworld from '../cover/images/westworld.jpg';
 const Gallery: React.FC = () => {
   return (
     <div className="gallery">
-      <div className="gallery-row">
-        <Cover
-          image={Daredevil}
-          id="daredevil"
-          title="Daredevil"
-          description="Action | Crime | Drama"
-        />
-        <Cover
-          image={MyNameIsEarl}
-          id="my-name-is-earl"
-          title="My Name Is Earl"
-          description="Comedy"
-        />
-        <Cover
-          image={Ozark}
-          id="ozark"
-          title="Ozark"
-          description="Crime | Drama | Thriller"
-        />
-      </div>
-      <div className="gallery-row">
-        <Cover
-          image={SweetHome}
-          id="sweet-home"
-          title="Sweet Home"
-          description="Action | Drama | Horror"
-        />
-        <Cover
-          image={TheMentalist}
-          id="the-mentalist"
-          title="The Mentalist"
-          description="Crime | Drama | Mystery"
-        />
-        <Cover
-          image={Westworld}
-          id="westworld"
-          title="Westworld"
-          description="Drama | Mystery | Sci-Fi"
-        />
-      </div>
+      <Cover
+        image={Daredevil}
+        id="daredevil"
+        title="Daredevil"
+        description="Action | Crime | Drama"
+      />
+      <Cover
+        image={MyNameIsEarl}
+        id="my-name-is-earl"
+        title="My Name Is Earl"
+        description="Comedy"
+      />
+      <Cover
+        image={Ozark}
+        id="ozark"
+        title="Ozark"
+        description="Crime | Drama | Thriller"
+      />
+      <Cover
+        image={SweetHome}
+        id="sweet-home"
+        title="Sweet Home"
+        description="Action | Drama | Horror"
+      />
+      <Cover
+        image={TheMentalist}
+        id="the-mentalist"
+        title="The Mentalist"
+        description="Crime | Drama | Mystery"
+      />
+      <Cover
+        image={Westworld}
+        id="westworld"
+        title="Westworld"
+        description="Drama | Mystery | Sci-Fi"
+      />
     </div>
   );
 };
