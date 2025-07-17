d472d461795f799cbb507facd081418f9d906b8a
Sule Tunkara <st.tunkara@gmail.com>
2025-07-17 15:40:42 +0100

    Introduce component state #13

--- src/details/Details.tsx
+++ src/details/Details.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Details.css';
 
-function Details() {
-  return (
-    <div>
-      <h1>Details Page</h1>
-      <p>This is the details page for a specific TV show.</p>
-    </div>
-  );
+interface DetailsState {
+  message: string;
+}
+
+class Details extends Component<{}, DetailsState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      message: 'This is the details page for a specific TV show.',
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ message: 'Coming Soon!' });
+    }, 3000);
+  }
+
+  render() {
+    return <h1>{this.state.message}</h1>;
+  }
 }
 
 export default Details;
