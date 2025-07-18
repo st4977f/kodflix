191bef8fb73be991fe9f78eb31d20ee0c0c9d232
Sule Tunkara <st.tunkara@gmail.com>
2025-07-18 12:36:18 +0100

    Return a list of shows from the backend #19

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import getShows from './shows';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/rest/shows', (req, res) => {
+  res.send(getShows());
 });
 
 app.listen(port, () => {
---
+++ src/backend/shows.ts
@@ -0,0 +1,48 @@
+function getShows() {
+  return [
+    {
+      id: 'daredevil',
+      title: 'Daredevil',
+      description: 'Action | Crime | Drama',
+      synopsis:
+        "A blind lawyer by day, vigilante by night, Matt Murdock fights crime in Hell's Kitchen as Daredevil.",
+    },
+    {
+      id: 'my-name-is-earl',
+      title: 'My Name Is Earl',
+      description: 'Comedy',
+      synopsis:
+        'A man seeks to improve his life by correcting all the wrongs he has done in the past.',
+    },
+    {
+      id: 'ozark',
+      title: 'Ozark',
+      description: 'Crime | Drama | Thriller',
+      synopsis:
+        'A financial planner relocates his family to the Ozarks after a money-laundering scheme goes wrong.',
+    },
+    {
+      id: 'sweet-home',
+      title: 'Sweet Home',
+      description: 'Drama | Horror | Thriller',
+      synopsis:
+        'Residents of a rundown apartment complex fight to survive as the world is plagued by monsters.',
+    },
+    {
+      id: 'the-mentalist',
+      title: 'The Mentalist',
+      description: 'Crime | Drama | Mystery',
+      synopsis:
+        'A former psychic medium uses his keen observational skills to solve crimes as a consultant for the California Bureau of Investigation.',
+    },
+    {
+      id: 'westworld',
+      title: 'Westworld',
+      description: 'Drama | Sci-Fi | Thriller',
+      synopsis:
+        'In a futuristic amusement park, guests interact with lifelike robots in a world where they can do anything without consequences.',
+    },
+  ];
+}
+
+export default getShows;
